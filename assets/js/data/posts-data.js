8d67171e191bda3388283c14399109fd5aa14dd8
Jawaria <jawaraasghar@gmail.com>
2026-08-04 13:31:38 +0500

    [admin] Update journal — by Mazahir

--- assets/js/data/posts-data.js
+++ assets/js/data/posts-data.js
@@ -1,17 +1,8 @@
 window.HR_POSTS = [
   {
     "date": "2026-08-04",
-    "title": "skardu menu ",
+    "title": "test 1 ",
     "body": "",
     "images": []
-  },
-  {
-    "date": "2026-08-04",
-    "title": "testing",
-    "body": "",
-    "images": [
-      "assets/images/journal/journal-1785831991970-0.jpg",
-      "assets/images/journal/journal-1785832153509-0.jpg"
-    ]
   }
 ];
