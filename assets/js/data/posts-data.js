401d25b48043b6303bacf063c8d319771416b6a3
Jawaria <jawaraasghar@gmail.com>
2026-08-04 13:35:34 +0500

    [admin] Update journal — by Mazahir

--- assets/js/data/posts-data.js
+++ assets/js/data/posts-data.js
@@ -1,14 +1,1 @@
-window.HR_POSTS = [
-  {
-    "date": "2026-08-04",
-    "title": "test 2 ",
-    "body": "",
-    "images": []
-  },
-  {
-    "date": "2026-08-04",
-    "title": "test 1 ",
-    "body": "",
-    "images": []
-  }
-];
+window.HR_POSTS = [];
